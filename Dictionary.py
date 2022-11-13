3141c1645d45ff1c27d5ceb947e8926eb8bab081
Fabson <fabson_sa@hotmail.com>
2022-11-13 18:57:31 -0300

    Fixed the upper/lower case no recognition

--- Dictionary.py
+++ Dictionary.py
@@ -3,6 +3,8 @@ vehicle={"sport":"Mustang", "city":"Aviator", "off-road":"Bronco", "truck":"F-15
 print("Entry the car category you want, then you will have the Ford related car")
 car=input()
 
+car=car.lower()
+
 selected=vehicle.get(car,"The search didin't find the car category you selected")
 
 print(selected)
